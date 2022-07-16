427ceb444018dbbd50ce2f1a31063e77cf4ee19f
Erikson <chris.s.erickson@gmail.com>
2022-07-16 17:41:11 -0400

    added the display portion

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-var translation ="";
-var h1=document.querySelector("#test");
 // giphy apiKey
 var apiKey = "yGaUUWmiT0fVGuECOupVZ3GOLKgW62fu"
 
-fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda&api_key=" + apiKey + "&limit=20")
+fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda+excited&api_key=" + apiKey + "&limit=5")
   .then(response => response.json())
   .then(json => {
     json.data
@@ -17,15 +15,15 @@ fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda&api_key=" + apiKey +
   .catch(error => document.body.appendChild = error)
 
 
-var text =
-  "Yoda translator API gives you API access to yoda translator, so that you can integrate this functionality in your websites or applications.";
+var text ="why is this not working";
 
 fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
+    console.log(data.contents)
+    console.log(data.contents.translated)    
+    document.getElementById("test").innerHTML=data.contents.translated;
   })
   .catch((err) => console.log(err));
-var h1s = document.createElement("span")
-h1s.textContent=translation;
-h1.appendChild(h1s);
+
