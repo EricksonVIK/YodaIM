0822132c844151e37f847ad86dc602d80818c744
Erikson <chris.s.erickson@gmail.com>
2022-07-16 15:19:06 -0400

    starting yodafetch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var translation ="";
+var h1=document.querySelector("#test");
 // giphy apiKey
 var apiKey = "yGaUUWmiT0fVGuECOupVZ3GOLKgW62fu"
 
@@ -15,3 +17,15 @@ fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda&api_key=" + apiKey +
   .catch(error => document.body.appendChild = error)
 
 
+var text =
+  "Yoda translator API gives you API access to yoda translator, so that you can integrate this functionality in your websites or applications.";
+
+fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
+var h1s = document.createElement("span")
+h1s.textContent=translation;
+h1.appendChild(h1s);
