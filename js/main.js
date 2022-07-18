8ba06c85941b48a9fe17233956d4ad1489238ff0
Erikson <chris.s.erickson@gmail.com>
2022-07-18 19:08:56 -0400

    added html holder and function for gig

--- js/main.js
+++ js/main.js
@@ -1,29 +1,34 @@
 // giphy apiKey
 var apiKey = "yGaUUWmiT0fVGuECOupVZ3GOLKgW62fu"
 
-fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda+excited&api_key=" + apiKey + "&limit=5")
-  .then(response => response.json())
-  .then(json => {
-    json.data
-      .map(gif => gif.images.fixed_height.url)
-      .forEach(url => {
-        let img = document.createElement('img')
-        img.src = url
-        document.body.appendChild(img)
-      })
+var yodaGif =function(){
+    fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda+excited&api_key=" + apiKey + "&limit=1")
+        .then(response => response.json())
+        .then(json => {
+            json.data
+            .map(gif => gif.images.fixed_height.url)
+        .forEach(url => {
+            var img = document.createElement('img')
+            img.src = url
+            var gifHolder = document.querySelector("#gif");
+            gifHolder.appendChild(img);
+        })
   })
   .catch(error => document.body.appendChild = error)
-
+};
 
 var text ="why is this not working";
 
-fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    console.log(data.contents)
-    console.log(data.contents.translated)    
-    document.getElementById("test").innerHTML=data.contents.translated;
-  })
-  .catch((err) => console.log(err));
+var yodaPhrase = function (){
+    fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        console.log(data.contents);
+        console.log(data.contents.translated);  
+    })
+    .catch((err) => console.log(err));
+};
 
+yodaGif();
+yodaPhrase();
