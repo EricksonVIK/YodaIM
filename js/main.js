d3b080ac19f839b21ac0cdd915836760c5e52935
Erikson <chris.s.erickson@gmail.com>
2022-07-18 21:09:53 -0400

    pulled text from input - but undefined still in function

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ var apiKey = "yGaUUWmiT0fVGuECOupVZ3GOLKgW62fu"
 // global
 var userInput =document.querySelector("#generate-gif");
 var phraseInput=document.querySelector("#phrase")
-var textSubmit=phraseInput.value.trim();
-var text =textSubmit;
+var text = [];
+
+function getInput(){
+    var enteredPhrase=phraseInput.value.trim();
+    alert(enteredPhrase);
+    text.push(enteredPhrase);
+}
+
+var submitForm = function(){
+    // prevent page from refreshing
+    event.preventDefault();
+    getInput();
+    yodaGif();
+    yodaPhrase();
+    // var enteredPhrase=phraseInput.value.trim();
+    // text.push(enteredPhrase);
+}
 
 
 var yodaGif =function(){
-    fetch("https://api.giphy.com/v1/gifs/search?q=starwars+yoda&api_key=" + apiKey + "&limit=1")
+    var gifApi = "https://api.giphy.com/v1/gifs/search?q=starwars+yoda&api_key=" + apiKey + "&limit=1";
+    fetch(gifApi)
         .then(response => response.json())
         .then(json => {
             json.data
@@ -24,25 +40,24 @@ var yodaGif =function(){
   .catch(error => document.body.appendChild = error)
 };
 
+// var text = phraseInput.value();
+// // var text="this is starting to piss me off"
 
 var yodaPhrase = function (){
-    fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        console.log(data.contents);
-        console.log(data.contents.translated);  
-        document.getElementById("finalPhrase").innerHTML=data.contents.translated;
-    })
+    console.log("called");
+    var phraseApi = "http://api.funtranslations.com/translate/yoda?text=" + text + "";
+    // fetch(`http://api.funtranslations.com/translate/yoda?text=${text}`)
+    fetch (phraseApi)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            console.log(data.contents);
+            console.log(data.contents.translated);  
+            document.getElementById("finalPhrase").textContent=data.contents.translated;
+        })
     .catch((err) => console.log(err));
 };
 
-var submitForm = function(){
-    // prevent page from refreshing
-    event.preventDefault();
-    yodaGif();
-    yodaPhrase();
-}
 
 // yodaGif();
 // yodaPhrase();
